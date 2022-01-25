4eedae486f02b42fdb6e9a94db499aedb5508477
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-01-25 16:00:46 +0000

    Second lab done

--- labs/2-conditionals.rb
+++ labs/2-conditionals.rb
@@ -13,6 +13,18 @@
 # that reads "YOU LOSE!", otherwise, write a message that
 # reads "THE POINT IS {number}"
 
+dice1=rand(1..6)
+dice2=rand(1..6)
+if dice1+dice2 == 7 || dice1+dice2 == 11
+    puts "YOU WIN!!"
+elsif dice1+dice2 == 2 || dice1+dice2 == 3 || dice1+dice2 == 12
+    puts "YOU LOSE"
+else puts "The point is #{dice1+dice2}."
+end
+
+puts dice1
+puts dice2
+
 # HINTS
 # Get a random number between 1 and 6 by doing:
 # rand(1..6)
