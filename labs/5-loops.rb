e5967723290c3b90af9fb4b798cf56df609bc1e9
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-01-25 20:06:07 +0000

    Inserted more complex way as per Lab solution for looping lab

--- labs/5-loops.rb
+++ labs/5-loops.rb
@@ -9,8 +9,30 @@
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
+#simple way that I did myself first
+
 for card1 in suits
     for card2 in ranks
         puts "#{card2} of #{card1}"
     end
 end
+
+#more complex way as per solution - principle is to assign the output to a variable
+
+#need to make empty deck first (forgot to do this first time) ([] to specify array)
+deck = []
+
+#loop
+for card_1 in suits
+    for card_2 in ranks
+        temp_card = "#{card_2} of #{card_1}"
+        deck.push(temp_card)
+    end
+end
+
+# named temp_card for emphasis. This is the temporary variable that feeds into the actual variable 
+# (deck) we are building through the loop
+
+puts deck
+
+puts deck[rand(1..52)]
