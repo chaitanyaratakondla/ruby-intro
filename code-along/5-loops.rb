92a7d66bc11d036d9da3b05f3cccf3be91e0303c
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-01-25 17:25:25 +0000

    Loops code along

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,8 +2,14 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
-things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts"]
+things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts","sandwiches","toast","fries","salads"]
 
 # The Old Way
 
 # The New Way
+for bacon in things_that_would_be_better_with_bacon
+    # 1st time, this will be tacos
+    puts "#{bacon} would be better with bacon! "
+    # 2nd time, this will be burgers
+    # and so on, until you get to salads
+end
