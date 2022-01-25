b5641980ff4a04f7236bbd86cb6513cb6139a87e
chaitanyaratakondla <chaitanya.ratakondla@kellogg.northwestern.edu>
2022-01-25 15:20:50 +0000

    First code along and lab

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,32 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
 
 # Perform simple math with numbers
+puts 5+2
 
 # Strings
+puts "Hello, world!"
 
 # Combine strings together
+puts "Tacos are" + "awesome"
+puts "Mu" + ("ha"*10) + " I'm a bad man"
+puts "Tacos" + 3.to_s
 
 # Variables
+y=3
+x = 10
+puts x*y
+
+food = "tacos"
+quantity = 3
+puts food*quantity
 
 # Combine strings and variables
+first_name = "Boba"
+greeting = "This is the Way, #{first_name}"
+puts greeting
 
 # String manipulation
+puts "Hello".reverse
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -9,6 +9,11 @@
 # dice. Set two variables, with names that make sense,
 # then write them out to the screen. Show the total
 # as well.
+dice_1 = rand(1..6)
+dice_2 = rand(1..6)
+total = dice_1 + dice_2
+puts "Your dice rolls are #{dice_1} and #{dice_2}. They add up to #{total}."
+puts "Your dice rolls are #{dice_1} and #{dice_2}. They add up to #{dice_1+dice_2}."
 
 # HINTS
 # Get a random number between 1 and 6 by doing:
